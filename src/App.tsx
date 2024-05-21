6e64f51f84e616175cfd538ecff4fe215ec337ec
Wojciech <weswojciech@gmail.com>
2024-05-21 14:10:54 +0200

    add functionality to change chosen button text

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import React, { useState } from 'react';
 import './App.scss';
+type Props = {
+  id: number;
+  addedIDS: number[];
+  handleClick: (id: number) => void;
+};
+const Item: React.FC<Props> = ({ id, addedIDS, handleClick }) => {
+  return (
+    <button onClick={() => handleClick(id)}>
+      {addedIDS.includes(id) ? `Add to cart` : `Added to cart`}
+    </button>
+  );
+};
+export const App = () => {
+  const [addedIDS, setAddedIDS] = useState<number[]>([]); //dodajemy telefony, ktorych przycisk został naciśnięty po ID
+  const handleClick = (id: number) => {
+    if (addedIDS.includes(id)) {
+      setAddedIDS(prev => prev.filter(searched => searched !== id));
+    } else {
+      setAddedIDS(prev => [...prev, id]);
+    }
+  };
 
-export const App = () => (
-  <div className="App">
-    <h1>Product Catalog</h1>
-  </div>
-);
+  return (
+    <div className="App">
+      <h1>Product Catalog</h1>
+      <Item id={2} addedIDS={addedIDS} handleClick={handleClick} />
+      <Item id={3} addedIDS={addedIDS} handleClick={handleClick} />
+    </div>
+  );
+};
