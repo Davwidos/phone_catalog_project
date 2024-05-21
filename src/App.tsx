7d5fc2d64ba911730d61b433d8d0afbf1b11e544
Wojciech <weswojciech@gmail.com>
2024-05-21 09:21:23 +0200

    add hover function as a useeffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import './App.scss';
-import './scripts/main';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,47 @@
+import { useEffect } from 'react';
 import Logo from '../../images/Logo.svg';
 import Slider from '../../images/slider-button-up.svg';
+import SliderHover from '../../images/slider-button-up_hover.svg';
 
 export const Footer = () => {
+  useEffect(() => {
+    const imageContainer = document.querySelector('.footer__scrollup');
+    const hoverImage = document.querySelector<HTMLImageElement>('.slider-icon');
+
+    const originalImageUrl = Slider;
+    const hoverImageUrl = SliderHover;
+
+    const handleMouseEnter = () => {
+      if (hoverImage) {
+        hoverImage.src = hoverImageUrl;
+        hoverImage.style.transform = 'rotate(-90deg)';
+      }
+    };
+
+    const handleMouseLeave = () => {
+      if (hoverImage) {
+        hoverImage.src = originalImageUrl;
+        hoverImage.style.transform = 'rotate(0deg)';
+      }
+    };
+
+    if (imageContainer) {
+      imageContainer.addEventListener('mouseenter', handleMouseEnter);
+      imageContainer.addEventListener('mouseleave', handleMouseLeave);
+    }
+
+    return () => {
+      if (imageContainer) {
+        imageContainer.removeEventListener('mouseenter', handleMouseEnter);
+        imageContainer.removeEventListener('mouseleave', handleMouseLeave);
+      }
+    };
+  }, []);
+
   return (
     <div className="footer">
       <a href="" className="footer__link link">
-        <img src={Logo} alt="" className="footer__logo logo" />
+        <img src={Logo} alt="Logo" className="footer__logo logo" />
       </a>
 
       <div className="footer__links">
