4695385bc1052ace8c5d25d8436824ed8f686751
Wojciech <weswojciech@gmail.com>
2024-05-21 17:18:34 +0200

    fix error with components inside <Routes>

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,18 @@ import { Page } from './components/Page';
 import { NotFoundPage } from './components/NotFoundPage';
 
 export const App = () => (
-  <Routes>
+  <>
     <Header />
-    <Route element={<Page />}>
-      <Route path="/" element={<h1>Home</h1>} />
-      <Route path="/phones" element={<h1>phones</h1>} />
-      <Route path="/tablets" element={<h1>tablets</h1>} />
-      <Route path="/accessories" element={<h1>accessories</h1>} />
-      <Route path="/home" element={<Navigate to="/" />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Route>
+    <Routes>
+      <Route element={<Page />}>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/phones" element={<h1>phones</h1>} />
+        <Route path="/tablets" element={<h1>tablets</h1>} />
+        <Route path="/accessories" element={<h1>accessories</h1>} />
+        <Route path="/home" element={<Navigate to="/" />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
     <Footer />
-  </Routes>
+  </>
 );
