ce9f9ee1205149c89f9f2e607084f334328b3dd0
Oles Partyka <oles.partyka.29@gmail.com>
2024-05-21 09:48:28 +0200

    mob card

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import './App.scss';
+import { ProductCard } from './components/ProductCard/ProductCard';
 
 export const App = () => (
   <div className="App">
     <h1>Product Catalog</h1>
+    <ProductCard />
   </div>
 );
