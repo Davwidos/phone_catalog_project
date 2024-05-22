63ffac422cbc1b85b6d8ebf0978799bd26b653d6
Davwidos <d.jurkowski015@gmail.com>
2024-05-22 16:09:26 +0200

    fixed issiues with footer and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import { Page } from './components/Page/Page';
-import BurgerMenu from './components/BurgerMenu/BurgerMenu';
 import { NotFoundPage } from './components/NotFoundPage/NotFoundPage';
 import { Cart } from './components/Cart/Cart';
 import { ProductList } from './components/ProductList/ProductList';
 
 export const App = () => (
-  <>
-    <BurgerMenu />
-    <Routes>
-      <Route element={<Page />}>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/phones" element={<ProductList />} />
-        <Route path="/tablets" element={<ProductList />} />
-        <Route path="/accessories" element={<ProductList />} />
-        <Route path="/home" element={<Navigate to="/" />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
-    </Routes>
-  </>
+  <Routes>
+    <Route element={<Page />}>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/phones" element={<ProductList />} />
+      <Route path="/tablets" element={<ProductList />} />
+      <Route path="/accessories" element={<ProductList />} />
+      <Route path="/home" element={<Navigate to="/" />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
+  </Routes>
 );
