ff6a34016da64c39c96a2feab4e1df6f298c0354
Rafał Górski <158020641+itsXenthis@users.noreply.github.com>
2024-05-22 09:35:07 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,9 @@ export const App = () => (
     <Routes>
       <Route element={<Page />}>
         <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/phones" element={<ProductList />} />
-        <Route path="/tablets" element={<ProductList />} />
-        <Route path="/accessories" element={<ProductList />} />
+        <Route path="/phones" element={<h1>phones</h1>} />
+        <Route path="/tablets" element={<h1>tablets</h1>} />
+        <Route path="/accessories" element={<h1>accessories</h1>} />
         <Route path="/home" element={<Navigate to="/" />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
