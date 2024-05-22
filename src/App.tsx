876a72318035d1dfc6645056095c02f9395526a7
Rafał Górski <158020641+itsXenthis@users.noreply.github.com>
2024-05-22 09:32:42 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.scss';
-import { Page } from './components/Page/Page';
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
+import { Page } from './components/Page';
 import { ProductList } from './components/ProductList/ProductList';
 import { NotFoundPage } from './components/NotFoundPage';
 
 export const App = () => (
-  <Routes>
-    <Route element={<Page />}>
-      <Route path="/" element={<h1>Home</h1>} />
-      <Route path="/phones" element={<ProductList />} />
-      <Route path="/tablets" element={<h1>tablets</h1>} />
-      <Route path="/accessories" element={<h1>accessories</h1>} />
-      <Route path="/home" element={<Navigate to="/" />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Route>
-  </Routes>
+  <>
+    <Header />
+    <Routes>
+      <Route element={<Page />}>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/phones" element={<ProductList />} />
+        <Route path="/tablets" element={<ProductList />} />
+        <Route path="/accessories" element={<ProductList />} />
+        <Route path="/home" element={<Navigate to="/" />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+    <Footer />
+  </>
 );
