8b16ce3c61c4c874d617baf4196856622a5bf423
Wojciech <weswojciech@gmail.com>
2024-05-22 12:49:00 +0200

    add styling

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -1,3 +1,6 @@
+import { ShopByCategory } from '../Shop-by-category/Shop-by-category';
+import './Homepage.scss';
+
 export const Homepage = () => {
   return (
     <div className="main">
@@ -7,26 +10,7 @@ export const Homepage = () => {
 
       <div className="main__slider new-models">Brand new models</div>
 
-      <div className="main__shop-by-category shop-by-category">
-        <div className="product-category">
-          <img className="product-category__image" src="" alt="mobile-phones" />
-
-          <p className="product-category__category">Mobile phones</p>
-          <p className="product-category__models">95 models</p>
-        </div>
-        <div className="product-category">
-          <img className="product-category__image" src="" alt="tablets" />
-
-          <p className="product-category__category">Tablets</p>
-          <p className="product-category__models">24 models</p>
-        </div>
-        <div className="product-category">
-          <img className="product-category__image" src="" alt="accessories" />
-
-          <p className="product-category__category">Accessories</p>
-          <p className="product-category__models">100 models</p>
-        </div>
-      </div>
+      <ShopByCategory />
 
       <div className="main__hot-prices">HOT PRICES</div>
     </div>
---
+++ src/components/Shop-by-category/Shop-by-category.tsx
@@ -0,0 +1,37 @@
+import './Shop-by-category.scss';
+import Phones from '../../images/main/product-category/Phones.svg';
+import Accessories from '../../images/main/product-category/Accessories.svg';
+import Tablets from '../../images/main/product-category/Tablets.svg';
+export const ShopByCategory = () => {
+  return (
+    <div className="main__shop-by-category shop-by-category">
+      <h1 className="shop-by-category__title">Shop by category</h1>
+      <div className="product-category">
+        <img
+          className="product-category__image"
+          src={Phones}
+          alt="mobile-phones"
+        />
+
+        <p className="product-category__category">Mobile phones</p>
+        <p className="product-category__models">95 models</p>
+      </div>
+      <div className="product-category">
+        <img className="product-category__image" src={Tablets} alt="tablets" />
+
+        <p className="product-category__category">Tablets</p>
+        <p className="product-category__models">24 models</p>
+      </div>
+      <div className="product-category">
+        <img
+          className="product-category__image"
+          src={Accessories}
+          alt="accessories"
+        />
+
+        <p className="product-category__category">Accessories</p>
+        <p className="product-category__models">100 models</p>
+      </div>
+    </div>
+  );
+};
