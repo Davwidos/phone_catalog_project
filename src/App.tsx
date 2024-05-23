eba1527c8c76c71b5af67a577f7218db1967eda5
Wojciech <weswojciech@gmail.com>
2024-05-23 12:26:47 +0200

    add route and removed from page(layout reason)

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Page } from './components/Page/Page';
 import { NotFoundPage } from './components/NotFoundPage/NotFoundPage';
 import { Cart } from './components/Cart/Cart';
 import { ProductList } from './components/ProductList/ProductList';
+import { FavouritesPage } from './components/FavouritesPage/FavouritesPage';
 export const App = () => (
   <Routes>
     <Route element={<Page />}>
@@ -11,6 +12,7 @@ export const App = () => (
       <Route path="/phones" element={<ProductList />} />
       <Route path="/tablets" element={<ProductList />} />
       <Route path="/accessories" element={<ProductList />} />
+      <Route path="/favourites" element={<FavouritesPage />} />
       <Route path="/home" element={<Navigate to="/" />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="*" element={<NotFoundPage />} />
