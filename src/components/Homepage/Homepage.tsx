122f725a7a86cc9a3daca653f79ca91fabb0d4a3
Rafał Górski <rafal.gorski@wp.pl>
2024-05-23 18:44:19 +0200

    slider-home-page-banners

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -1,11 +1,12 @@
 import { ShopByCategory } from '../Shop-by-category/Shop-by-category';
+import SliderHomePage from '../SliderHomePage/SliderHomePage';
 import './Homepage.scss';
 
 export const Homepage = () => {
   return (
     <div className="main">
       <h1 className="main__title title">Welcome to Nice Gadgets store!</h1>
-      <div className="main__slider slider">slider</div>
+      <SliderHomePage />
       <div className="main__slider new-models">Brand new models</div>
       <ShopByCategory />
       <div className="main__hot-prices">HOT PRICES</div>
---
+++ src/components/SliderHomePage/SliderHomePage.jsx
@@ -0,0 +1,60 @@
+/* eslint-disable max-len */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable import/no-extraneous-dependencies */
+import React, { useState } from 'react';
+import { useSwipeable } from 'react-swipeable';
+import './SliderHomePage.scss';
+import bannerAccessories from '../../images/slidesHomePage/banner-accessories.png';
+import bannerPhones from '../../images/slidesHomePage/banner-phones.png';
+import bannerTablets from '../../images/slidesHomePage/banner-tablets.png';
+
+const Slider = () => {
+  const slides = [
+    { image: bannerAccessories, alt: 'Accessories' },
+    { image: bannerPhones, alt: 'Phones' },
+    { image: bannerTablets, alt: 'Tablets' },
+  ];
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  const handlers = useSwipeable({
+    onSwipedLeft: nextSlide,
+    onSwipedRight: prevSlide,
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: true,
+  });
+
+  return (
+    <div className="slider-container" {...handlers}>
+      {slides.map((slide, index) => (
+        <div
+          className={index === current ? 'slide active' : 'slide'}
+          key={index}
+        >
+          {index === current && (
+            <img src={slide.image} alt={slide.alt} className="image" />
+          )}
+        </div>
+      ))}
+      <div className="dots">
+        {slides.map((_, index) => (
+          <span
+            key={index}
+            className={index === current ? 'dot active' : 'dot'}
+            onClick={() => setCurrent(index)}
+          ></span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
