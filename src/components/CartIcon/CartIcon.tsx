cd13bdccdee640179d9bea6a80b67ca73a105918
Wojciech <weswojciech@gmail.com>
2024-05-27 13:50:38 +0200

    solution

--- src/components/CartIcon/CartIcon.tsx
+++ src/components/CartIcon/CartIcon.tsx
@@ -1,8 +1,16 @@
 import cartIcon from '../../icons/shopbag-icon.svg';
+import { useCart } from '../../provider/CartProvider';
 import './CartIcon.scss';
 
 export const CartIcon = () => {
+  const { cartItems } = useCart();
+
   return (
-    <img src={cartIcon} alt="favourites-icon" className="icon-container" />
+    <div className="icon-wrapper">
+      <img src={cartIcon} alt="cart-icon" className="icon-container" />
+      {cartItems.length !== 0 && (
+        <div className="icon-container--amount">{cartItems.length}</div>
+      )}
+    </div>
   );
 };
--- src/components/FavoruitesIcon/FavouritesIcon.tsx
+++ src/components/FavoruitesIcon/FavouritesIcon.tsx
@@ -1,7 +1,14 @@
 import favouritesIcon from '../../icons/favorite-icon.svg';
+import { useFavourites } from '../../provider/FavouritesProvider';
 
 export const FavouritesIcon = () => {
+  const { favouritesIDS } = useFavourites();
   return (
-    <img src={favouritesIcon} alt="cart-icon" className="icon-container" />
+    <div className="icon-wrapper">
+      <img src={favouritesIcon} alt="cart-icon" className="icon-container" />
+      {favouritesIDS.length !== 0 && (
+        <div className="icon-container--amount">{favouritesIDS.length}</div>
+      )}
+    </div>
   );
 };
