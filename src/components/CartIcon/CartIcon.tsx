52f7ffda76db2da903ee4eb1811a25617a9a2596
Wojciech <weswojciech@gmail.com>
2024-05-27 16:32:53 +0200

    fix font size for 99+ items

--- src/components/CartIcon/CartIcon.tsx
+++ src/components/CartIcon/CartIcon.tsx
@@ -9,7 +9,13 @@ export const CartIcon = () => {
     <div className="icon-wrapper">
       <img src={cartIcon} alt="cart-icon" className="icon-container" />
       {cartItems.length !== 0 && (
-        <div className="icon-container--amount">
+        <div
+          className={
+            cartItems.length <= 99
+              ? 'icon-container--amount'
+              : 'icon-container--amount tooManyItems'
+          }
+        >
           {cartItems.length <= 99 ? cartItems.length : '99+'}
         </div>
       )}
