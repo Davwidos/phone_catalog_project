62cd66bd10eff591d6e24ed1226b33276ca93be9
Rafał Górski <158020641+itsXenthis@users.noreply.github.com>
2024-05-27 15:17:07 +0200

    Update FavouritesPage.tsx

--- src/components/FavouritesPage/FavouritesPage.tsx
+++ src/components/FavouritesPage/FavouritesPage.tsx
@@ -1,12 +1,13 @@
 import { ProductCard } from '../ProductCard/ProductCard';
+import { Breadcrumbs } from '../Breadcrumbs/Breadcrumbs';
 import './FavouritesPage.scss';
 
 export const FavouritesPage = () => {
   return (
     <div className="favouritesPage">
-      {/* <Breadcrumbs /> */} breadcrumbsplaceholder
+      <Breadcrumbs path="Favourites" />
       <h1 className="favouritesPage__title">Favourites</h1>
-      <p className="favouritesPage__text"> *placeholder* items</p>
+      <p className="favouritesPage__text">*placeholder* items</p>
       <div className="favouritesPage__products">
         <ProductCard id={0} price={10} />
         <ProductCard id={0} price={10} />
