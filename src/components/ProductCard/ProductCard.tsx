fbc32cf5a4694323dbfebf741efdb7a2e1ad32fe
Oles Partyka <oles.partyka.29@gmail.com>
2024-05-27 16:47:57 +0200

    ProductCard fixed favorites button + tablet and desktop

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -26,7 +26,6 @@ export const ProductCard: React.FC<Props> = ({ id, price, width }) => {
 
   return (
     <div className="productCard" style={cardStyles}>
-      {id}
       <img
         className="productCard__img"
         src={img}
@@ -43,56 +42,50 @@ export const ProductCard: React.FC<Props> = ({ id, price, width }) => {
         </Link>
       </h2>
 
-      <div className="productCard__bottom-content">
-        <div className="productCard__prices">
-          $799
-          <div className="productCard__old-price">
-            $999
-            <span className="productCard__line-through">$999</span>
-          </div>
+      <div className="productCard__prices">
+        $799
+        <div className="productCard__old-price">
+          $999
+          <span className="productCard__line-through">$999</span>
         </div>
+      </div>
 
-        <div className="productCard__divider" />
+      <div className="productCard__info-row">
+        Screen
+        <span className="productCard__info-value">6.1” OLED</span>
+      </div>
 
-        <div className="productCard__info">
-          <div className="productCard__info-row">
-            Screen
-            <span className="productCard__info-value">6.1” OLED</span>
-          </div>
+      <div className="productCard__info-row">
+        Capacity
+        <span className="productCard__info-value">128 GB</span>
+      </div>
 
-          <div className="productCard__info-row">
-            Capacity
-            <span className="productCard__info-value">128 GB</span>
-          </div>
+      <div className="productCard__info-row">
+        RAM
+        <span className="productCard__info-value">6 GB</span>
+      </div>
 
-          <div className="productCard__info-row">
-            RAM
-            <span className="productCard__info-value">6 GB</span>
-          </div>
-        </div>
+      <div className="productCard__btns">
+        <button
+          type="button"
+          className="productCard__addToCart productCard__btn"
+          onClick={() => handleAddToCart({ id, price })}
+        >
+          Add to cart
+        </button>
 
-        <div className="productCard__btns">
-          <button
-            type="button"
-            className="productCard__addToCart productCard__btn"
-            onClick={() => handleAddToCart({ id, price })}
-          >
-            Add to cart
-          </button>
-
-          <button
-            type="button"
-            className="
-            productCard__favorites
-            productCard__btn"
-            onClick={() => handleAddToFavourites(id)}
-          >
-            <img
-              src={favouritesIDS.includes(id) ? favoritesRed : favorites}
-              alt="favorites"
-            />
-          </button>
-        </div>
+        <button
+          type="button"
+          className="
+             productCard__favorites
+             productCard__btn"
+          onClick={() => handleAddToFavourites(id)}
+        >
+          <img
+            src={favouritesIDS.includes(id) ? favoritesRed : favorites}
+            alt="favorites"
+          />
+        </button>
       </div>
     </div>
   );
