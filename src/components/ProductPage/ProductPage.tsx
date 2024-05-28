f777ebc93a2229bb69ae789ff591098d39aab024
Wojciech <weswojciech@gmail.com>
2024-05-28 12:57:00 +0200

    add filtering to provider and silders

--- src/components/ProductPage/ProductPage.tsx
+++ src/components/ProductPage/ProductPage.tsx
@@ -10,7 +10,7 @@ import { useProducts } from '../../provider/ProductsProvider';
 
 export const ProductPage: FC = () => {
   const { details } = useProductDetails();
-  const { products } = useProducts();
+  const { products, youMayAlsoLikeProducts } = useProducts();
 
   const product = useMemo(
     () => products.find(p => p.itemId === details?.id),
@@ -47,7 +47,7 @@ export const ProductPage: FC = () => {
 
       <div className="ProductPage__slider">
         <h1 className="title-slider">You may also like</h1>
-        <Slider />
+        <Slider models={youMayAlsoLikeProducts} />
       </div>
     </div>
   );
