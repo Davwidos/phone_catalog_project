665a31af1c11074bc6b3b2f301972b1901775403
Wojciech <weswojciech@gmail.com>
2024-05-31 11:54:40 +0200

    fixed grid breakpoints and minor visual card issue

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -25,76 +25,81 @@ export const ProductCard: React.FC<Props> = ({ product, width }) => {
 
   return (
     <div className="productCard" style={cardStyles}>
-      <img
-        className="productCard__img"
-        src={product.image}
-        alt={product.name}
-      />
+      <div className="productCard__container">
+        <img
+          className="productCard__img"
+          src={product.image}
+          alt={product.name}
+        />
 
-      <h2 className="productCard__title">
-        <Link
-          to={`/${product.category}/${product.itemId}`}
-          state={{ product }}
-          onClick={handleGoToUpPage}
-        >
-          {product.name}
-        </Link>
-      </h2>
-
-      <div className="productCard__prices">
-        {product.price}
-        <div className="productCard__old-price">
-          {product.fullPrice}
-          <span className="productCard__line-through">{product.fullPrice}</span>
+        <h2 className="productCard__title">
+          <Link
+            to={`/${product.category}/${product.itemId}`}
+            state={{ product }}
+            onClick={handleGoToUpPage}
+          >
+            {product.name}
+          </Link>
+        </h2>
+      </div>
+      <div className="productCard__container">
+        <div className="productCard__prices">
+          ${product.price}
+          <div className="productCard__old-price">
+            ${product.fullPrice}
+            <span className="productCard__line-through">
+              ${product.fullPrice}
+            </span>
+          </div>
         </div>
-      </div>
+        <div className="productCard__info--container">
+          <div className="productCard__info-row">
+            Screen
+            <span className="productCard__info-value">{product.screen}</span>
+          </div>
 
-      <div className="productCard__info-row">
-        Screen
-        <span className="productCard__info-value">{product.screen}</span>
-      </div>
+          <div className="productCard__info-row">
+            Capacity
+            <span className="productCard__info-value">{product.capacity}</span>
+          </div>
 
-      <div className="productCard__info-row">
-        Capacity
-        <span className="productCard__info-value">{product.capacity}</span>
-      </div>
+          <div className="productCard__info-row">
+            RAM
+            <span className="productCard__info-value">{product.ram}</span>
+          </div>
+        </div>
+        <div className="productCard__btns">
+          <button
+            type="button"
+            className={`productCard__addToCart productCard__btn + ${
+              cartItems.some(i => i.id === product.id)
+                ? 'Button--primary active'
+                : ''
+            }`}
+            onClick={() => toggleAddToCart(product)}
+          >
+            {cartItems.some(i => i.id === product.id)
+              ? 'Added to cart'
+              : 'Add to cart'}
+          </button>
 
-      <div className="productCard__info-row">
-        RAM
-        <span className="productCard__info-value">{product.ram}</span>
-      </div>
-
-      <div className="productCard__btns">
-        <button
-          type="button"
-          className={`productCard__addToCart productCard__btn + ${
-            cartItems.some(i => i.id === product.id)
-              ? 'Button--primary active'
-              : ''
-          }`}
-          onClick={() => toggleAddToCart(product)}
-        >
-          {cartItems.some(i => i.id === product.id)
-            ? 'Added to cart'
-            : 'Add to cart'}
-        </button>
-
-        <button
-          type="button"
-          className="
+          <button
+            type="button"
+            className="
              productCard__favorites
              productCard__btn"
-          onClick={() => handleAddToFavourites(product)}
-        >
-          <img
-            src={
-              favourites.some(p => p.id === product.id)
-                ? favoritesRed
-                : favorites
-            }
-            alt="favorites"
-          />
-        </button>
+            onClick={() => handleAddToFavourites(product)}
+          >
+            <img
+              src={
+                favourites.some(p => p.id === product.id)
+                  ? favoritesRed
+                  : favorites
+              }
+              alt="favorites"
+            />
+          </button>
+        </div>
       </div>
     </div>
   );
