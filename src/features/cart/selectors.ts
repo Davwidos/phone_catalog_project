5b5b48129a3cb899c6a6e8e5cb9a4097ad9fbf7e
Wojciech <weswojciech@gmail.com>
2024-06-03 19:16:54 +0200

    implement reducer

--- src/features/cart/selectors.ts
+++ src/features/cart/selectors.ts
@@ -6,3 +6,8 @@ export const selectCartValue = createSelector(
   cart =>
     cart.reduce((prev, curr) => prev + curr.price * (curr.amount || 0), 0),
 );
+
+export const selectTotalItems = createSelector(
+  [(state: RootState) => state.cart],
+  cartItems => cartItems.reduce((total, item) => total + (item.amount || 0), 0),
+);
